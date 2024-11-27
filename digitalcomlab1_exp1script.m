0fa7c377dd7b09d4c18441a9f5f74ced98b67048
Trevor <eng2190302021@mmu.ac.ke>
2024-11-27 12:01:40 +0000

    'Simulating quantization and quantization error'

--- digitalcomlab1_exp1script.m
+++ digitalcomlab1_exp1script.m
@@ -1,6 +1,6 @@
-clear all;
-close all;
-clc;
+%clear all;
+%close all;
+%clc;
 
 tot=1;
 td=0.002;
@@ -49,4 +49,32 @@ plot(Faxisu,abs(Xfftu));
 xlabel('Frequency(Hz)');
 ylabel('Magnitude');
 title('Spectrum of Sampled Signal');
+grid on;
+
+%Define quantization levels
+levels=16;
+x_min=min(x_sampled);
+x_max=max(x_sampled);
+step=(x_max-x_min)/levels;
+
+%Quantize the sampled signal
+x_quantized=step*round((x_sampled-x_min)/step)+x_min;
+
+%Plot Quantized vs. sampled signal
+figure;
+stem(n,x_sampled,'r','LineWidth',1.5);hold on;
+stem(n,x_quantized,'b--','LineWidth',1.5);
+xlabel('Time(s)');
+ylabel('Amplitude');
+title('Sampled Signal vs. Quantized Signal');
+legend('Sampled signal','Quantized Signal');
+grid on;
+
+%Quantization error
+quantization_error=x_sampled-x_quantized;
+figure;
+stem(n,quantization_error,'LineWidth',1.5);
+xlabel('Time(s)');
+ylabel('Error');
+title('Quantization Error');
 grid on;
